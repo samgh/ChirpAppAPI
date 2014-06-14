1ae113d3f363de38f18ea4e080ca2a9a897b0cbf
Sam Gavis-Hughson <sgavis@princeton.edu>
2014-06-14 18:30:47 -0400

    I feel like i'm getting close

--- application.py
+++ application.py
@@ -3,15 +3,15 @@ from flask import Flask, jsonify, url_for
 
 application = Flask(__name__)
 
-@app.route('/chirpapp/api/v1.0/<string:tweet>', methods = ['GET'])
+@application.route('/chirpapp/api/v1.0/<string:tweet>', methods = ['GET'])
 def get_new_tweet(tweet):
 	return jsonify( {'tweet' : tweet})
 
-@app.route('/')
+@application.route('/')
 def get_uri():
 	return jsonify({'uri' : url_for('get_new_tweet', tweet="URL_ENCODED_TWEET", _external = True)})
 
-@app.errorhandler(404)
+@application.errorhandler(404)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
